60c5b5fd7c9fecb2d59b5e10faf73e2860fc57b2
Nabile01 <nabil.elazizi@hotmail.fr>
2021-08-18 13:42:17 +0200

    classe + function

--- social.php
+++ social.php
@@ -1,53 +1,78 @@
 <?php
+
 /**
-*Plugin Name: SocialPlug
-*Description: Akecoucou
-*Version: 1.0
-*Author: Matisse Blasco
-**/
+ *Plugin Name: SocialExtension
+ *Description: Akecoucou
+ *Version: 1.0
+ *Author: Matisse Blasco
+ **/
 
-//Add social plugin after the opening body
-add_action('wp_body_open', 'social_head');
+require_once('class.social-widget.php');
 
-function get_user_or_websitename()
+
+
+function Socialnetwork_register_widget()
 {
-    if(!is_user_logged_in())
-    {
-        return 'Welcome to ' . get_bloginfo('name');
-    }
-    else
-    {
-        $current_user = wp_get_current_user();
-        return 'Welcome back ' . $current_user -> user_login;
-    }
+    register_widget('Socialnetwork');
+}
+add_action('widgets_init', 'Socialnetwork_register_widget');
+
+//création de table en bdd
+function table_install()
+{
+    global $wpdb;
+
+    $table_name = $wpdb->prefix . "socialnetwork";
+
+    $charset_collate = $wpdb->get_charset_collate();
+
+    $sql = "CREATE TABLE $table_name (
+    id mediumint(9) NOT NULL AUTO_INCREMENT,
+    name varchar(255) NOT NULL,
+    url varchar(255) DEFAULT '' NOT NULL,
+    PRIMARY KEY  (id)
+    ) $charset_collate;";
+
+    require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+    dbDelta($sql);
 }
 
-function social_head()
-{
-    echo '<h3 class="tb">' . get_user_or_websitename() . '</h3>';
+register_activation_hook(__FILE__, 'table_install');
+
+
+//FORMULAIRE
+function form_html() { ?>
+    <div class="wrap top-bar-wrapper">
+        <form method="post" action="options.php">
+            <?php settings_errors() ?>
+            <?php settings_fields('topbar_option_group'); ?>
+            <label for="topbar_field_eat">Nom du réseau:</label>
+            <input name="topbar_field" id="topbar_field_eat" type="text" value=" <?php echo get_option('topbar_field'); ?> ">
+            <?php submit_button(); ?>
+        </form>
+    </div>
+<?php 
 }
-//Add CSS to the social plugin
-add_action('wp_print_styles', 'social_css');
+    
+add_action('admin_head', 'form_html');
 
-function social_css()
-{
-    echo '
-    <style>
-    h3.tb {color: #fff; margin: 0; padding: 30px; text-align: center; background: blue}
-    </style>
-    ';
-}
-
-//Social plugin page
-
-function social_plugin_page() {
-    $page_title = 'Social Plugin Options';
-    $menu_title = 'Social Plugin';
+//afficher le formulaire dans le backofice
+function topbar_plugin_page() {
+    $page_title = 'Top Bar Options';
+    $menu_title = 'Socialnetwork';
     $capatibily = 'manage_options';
-    $slug = 'social-plugin';
-    $position = 60;
-
-    add_menu_page($page_title, $menu_title, $capatibily, $slug, $position);
+    $slug = 'another-socialnetwork-plugin';
+    $callback = 'form_html';
+    $icon = 'dashicons-schedule';
+    $position = 90;
+    
+    add_menu_page($page_title, $menu_title, $capatibily, $slug, $callback, $icon, $position);
 }
-
-add_action('admin_menu', 'social_plugin_page');
+    
+add_action('admin_menu', 'topbar_plugin_page');
+    
+function topbar_register_settings() {
+    register_setting('topbar_option_group', 'topbar_field');
+}
+    
+add_action('admin_init', 'topbar_register_settings');
